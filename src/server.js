a49fff242a5f9bd613aad8d49ce6467f26e1a7e5
Arthur Bordenyuk <arthur.bordenyuk@gmail.com>
2020-05-27 18:24:06 +0300

    Adds koa templating engine

--- src/server.js
+++ src/server.js
@@ -1,13 +1,26 @@
 const Koa = require("koa");
 const router = require("@koa/router")();
+const render = require("koa-ejs");
+const path = require("path");
 const app = new Koa();
 
-router.get("/", async (ctx) => {
-  ctx.body = "Hello, world!";
+render(app, {
+  root: path.join(__dirname, "resources", "views"),
+  layout: false,
+  viewExt: "html",
+  cache: false,
+  debug: process.env.NODE_ENV === "development",
 });
 
+router.get("/", async (ctx) => {
+  await ctx.render("index");
+});
 
 router.post('/donate', async (ctx) => {
+  // donate amount > 0
+  // donate currency in currencies
+  // create document in mongodb donations
+
   ctx.body = { ok: true };
 });
 
